f12ed13df7f66b352203068da9689456f2eb2779
hyzic <isaac.oghenevwapo@gmail.com>
2024-08-22 12:21:03 -0400

    Added Home, Stadium and Venue to App

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { VenueComponent } from './venue/venue.component';
+import { StadiumsComponent } from './stadiums/stadiums.component';
+import { LoginComponent } from './login/login.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    HomeComponent,
+    VenueComponent,
+    StadiumsComponent,
+    LoginComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { StadiumsComponent } from './stadiums/stadiums.component';
+import { VenueComponent } from './venue/venue.component';
+import { LoginComponent } from './login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: "", pathMatch: "full", redirectTo: "/login"},
+    {path: "home", component: HomeComponent},
+    {path: "venue", component: VenueComponent},
+    {path: "locations", component: StadiumsComponent},
+    {path: "login", component: LoginComponent},
+];
