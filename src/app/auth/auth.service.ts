fe7a61ef5a510d97e414936546241f6566f39b39
hyzic <isaac.oghenevwapo@gmail.com>
2024-08-23 12:47:33 -0400

    Added methods for Login, Logout and IsLoggedIn in AuthService

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
+import { tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -11,7 +12,26 @@ export class AuthService {
   httpClient = inject(HttpClient);
   baseUrl = 'http://localhost:3000/api';
 
+  //SignUp
   signup(data: any){
     return this.httpClient.post(`${this.baseUrl}/register`,  data);   
+  }
+
+  //Login
+  login(data: any){
+    return this.httpClient.post(`${this.baseUrl}/login`, data)
+              .pipe(tap((result) => {
+                localStorage.setItem('authUser', JSON.stringify(result));
+              }));
+  }
+
+  //Logout
+  logout(){
+    localStorage.removeItem('authUser');
+  }
+
+  //IsLoggedIn
+  isLoggedIn(){
+    return localStorage.getItem('authUser') !== null;
   }
 }
